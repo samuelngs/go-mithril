7bcd9e13c6bfd3f942f54a4ff8421b7ee0fcf7a5
Samuel <sam@infinitely.io>
2015-12-28 23:39:59 -0700

    code clean up

--- element.go
+++ element.go
@@ -1,5 +1,10 @@
 package mithril
 
+var (
+	// SelfClosingTagToken is a list of self closing elements tag
+	SelfClosingTagToken = []string{"area", "base", "br", "col", "command", "embed", "hr", "img", "input", "keygen", "link", "meta", "param", "source", "track", "wbr", "!doctype"}
+)
+
 // VirtualElement is an object which can convert to real DOM element
 type VirtualElement struct {
 	Tag      string
@@ -10,4 +15,9 @@ type VirtualElement struct {
 // ID returns the attribute ID of the VirtualElement instance
 func (el *VirtualElement) ID() string {
 	return el.Attrs.ID
+}
+
+// String returns the html string of the element
+func (el *VirtualElement) String() string {
+	return ""
 }
