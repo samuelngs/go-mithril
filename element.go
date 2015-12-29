1e6c00c2e818f6ede3fa81d72734e840be96d30a
Samuel Ng <sam@infinitely.io>
2015-12-29 14:13:11 -0700

    VirtualElement to html string

--- element.go
+++ element.go
@@ -1,5 +1,7 @@
 package mithril
 
+import "fmt"
+
 var (
 	// SelfClosingTagToken is a list of self closing elements tag
 	SelfClosingTagToken = []string{"area", "base", "br", "col", "command", "embed", "hr", "img", "input", "keygen", "link", "meta", "param", "source", "track", "wbr", "!doctype"}
@@ -39,7 +41,55 @@ func (el *VirtualElement) HasAttr(key string) bool {
 	return false
 }
 
+// IsSelfClosingTag returns true if Tag is a self closing tag element
+func (el *VirtualElement) IsSelfClosingTag() bool {
+	for _, val := range SelfClosingTagToken {
+		if val == el.Tag {
+			return true
+		}
+	}
+	return false
+}
+
 // String returns the html string of the element
 func (el *VirtualElement) String() string {
-	return ""
+	res := make(chan string)
+	go func() {
+		html := "<"
+		if el.Tag != "" {
+			html += el.Tag
+		} else {
+			html += "div"
+		}
+		for _, attr := range el.Attrs {
+			html += (" " + attr.Key())
+			if len(attr.Val()) > 0 {
+				html += ("=\"" + attr.Val() + "\"")
+			}
+		}
+		if el.IsSelfClosingTag() {
+			html += " />"
+		} else {
+			html += ">"
+			switch obj := el.Children.(type) {
+			case *VirtualElement:
+				html += obj.String()
+			case string:
+				html += obj
+			case int, int32, int64:
+				html += fmt.Sprintf("%d", obj)
+			case float32, float64:
+				html += fmt.Sprintf("%f", obj)
+			}
+			html += "</"
+			if el.Tag != "" {
+				html += el.Tag
+			} else {
+				html += "div"
+			}
+			html += ">"
+		}
+		res <- html
+	}()
+	return <-res
 }
