d35572621ed70118af1a244cf3d65e2fa8364825
Samuel Ng <sam@infinitely.io>
2015-12-29 16:13:14 -0700

    new component class

--- component.go
+++ component.go
@@ -1,8 +1,13 @@
 package mithril
 
+// ExtendComponent creates a basic component view
+func ExtendComponent() *Component {
+	return &Component{make(map[string]interface{})}
+}
+
 // Component contains a controller and a view properties.
 type Component struct {
-	data map[string]interface{}
+	controller map[string]interface{}
 }
 
 // Controller function creates map for View
@@ -11,7 +16,17 @@ func (component *Component) Controller() {
 }
 
 // View function creates VirtualElement or string, int, bool
-func (component *Component) View() []interface{} {
+func (component *Component) View() interface{} {
 	panic("please override View function")
 	return nil
+}
+
+// Get returns controller value
+func (component *Component) Get(key string) interface{} {
+	return component.controller[key]
+}
+
+// Set value to controller scope
+func (component *Component) Set(key string, val interface{}) {
+	component.controller[key] = val
 }
